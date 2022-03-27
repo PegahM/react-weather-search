584f0006948c0d8915aafbfa1719edcbf478f61b
PegahM <pegahh.monzeri@gmail.com>
2022-03-28 01:07:27 +0430

    added GitHub info

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Pegah Monzeri and is{" "}
+        <a
+          href="https://github.com/PegahM/react-weather-search"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
