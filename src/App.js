d5f6b9a40491b2062217e7163a0f99ee158ed1c0
PegahM <pegahh.monzeri@gmail.com>
2022-03-28 11:56:24 +0430

    added rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default function App() {
         <a
           href="https://github.com/PegahM/react-weather-search"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on GitHub
         </a>
