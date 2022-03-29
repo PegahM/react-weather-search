bdb1435344eca52b9408178ee6724191a17b432c
PegahM <pegahh.monzeri@gmail.com>
2022-03-29 11:24:46 +0430

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Pegah Monzeri and is{" "}
-        <a
-          href="https://github.com/PegahM/react-weather-search"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Pegah Monzeri and is{" "}
+          <a
+            href="https://github.com/PegahM/react-weather-search"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
