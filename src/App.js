33d648b758dfbc2c238a51150e9c7acb03058713
PegahM <pegahh.monzeri@gmail.com>
2022-04-01 01:07:31 +0430

    Fixed the Weather icon

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="London" />
+        <Weather defaultCity="Tehran" />
         <footer>
           This project was coded by Pegah Monzeri and is{" "}
           <a
